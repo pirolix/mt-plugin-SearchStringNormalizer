90c703800ccc68e037c1b7fd5531ae79f5953039
piroli <piroli@adbff654-c4ea-448a-8ae3-cad037306009>
2012-04-20 04:04:56 +0000

    * 初版公開
    git-svn-id: svn://svn.magicvox.net/var/svn/mt-plugins/SearchStringNormalizer/trunk@208 adbff654-c4ea-448a-8ae3-cad037306009

---
+++ SearchStringNormalizer.pl
@@ -0,0 +1,55 @@
+package MT::Plugin::OMV::Search::SearchStringNormalizer;
+
+use strict;
+use MT::App::Search;
+
+use vars qw( $MYNAME $VERSION );
+$MYNAME = (split /::/, __PACKAGE__)[-1];
+$VERSION = '0.01_00';
+
+use base qw( MT::Plugin );
+my $plugin = __PACKAGE__->new ({
+    id => $MYNAME,
+    key => $MYNAME,
+    name => $MYNAME,
+    version => $VERSION,
+    author_name => 'Open MagicVox.net',
+    author_link => 'http://www.magicvox.net/',
+    plugin_link => 'http://www.magicvox.net/',#blog
+    doc_link => 'http://www.magicvox.net/',#trac
+    description => <<'HTMLHEREDOC',
+HTMLHEREDOC
+});
+MT->add_plugin( $plugin );
+
+
+
+MT::App::Search->add_callback ('prepare_throttle', 9, $plugin, \&_hdlr_prepare_throttle);
+sub _hdlr_prepare_throttle {
+    my ($cb, $app) = @_;
+
+    defined $app->param('normalize') && $app->param('normalize') == 0
+        and return 1;
+    my $search_string = $app->param('searchTerms') || $app->param('search')
+        or return 1;
+
+    # 全角空白＋全角英数字 → 半角
+    my $fullwidthchars = Encode::decode ('utf8',
+        '　！”＃＄％＆’（）＊＋，－．／'.
+        '０１２３４５６７８９：；＜＝＞？'.
+        '＠ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯ'.
+        'ＰＱＲＳＴＵＶＷＸＹＺ［￥］＾＿'.
+        '｀ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏ'.
+        'ｐｑｒｓｔｕｖｗｘｙｚ｛｜｝～');
+    # tr は変数を受けとれないので eval
+    eval "\$search_string =~ tr/$fullwidthchars/ -~/";
+    $search_string =~ s!^\s+|\s+$!!g;
+    $search_string =~ s!\s+! !g;
+
+    # 検索クエリを上書き
+    $app->param('search', $search_string);
+    $app->param('searchTerms', undef);
+    return 1;
+}
+
+1;
